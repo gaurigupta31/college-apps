70a2d4afd9c16dada2f4e58ca54b8d1c528824f2
gaurigupta31 <gaurigupta.315@gmail.com>
2022-04-10 10:47:34 +0530

    notion authentication :grey_question:

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, session, redirect, request, render_template
+from h11 import CLIENT
+from requests_oauthlib import OAuth2Session
 app = Flask(__name__)
+
+CLIENT_ID = os.getenv('NOTION_CLENT_ID')
+CLIENT_SECRET =  os.getenv('NOTION_API_KEY')
+BASE_NOTION_API_URL = "https://api.notion.com"
+NOTION_AUTHORIZE_URL = "https://api.notion.com/v1/oauth/authorize"
+REDIRECT_URI = "https://chief-fortune-judo.glitch.me/notion-auth"
+TOKEN_URL = "https://api.notion.com/v1/oauth/token"
+
 
 @app.route('/')
 def hello_world():
@@ -7,7 +18,14 @@ def hello_world():
 
 @app.route('/notion-auth')
 def notion_auth():
-   return 'Hello Notion'
+   notion = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI)
+   token = notion.fetch_token(
+        TOKEN_URL,
+        client_secret = CLIENT_SECRET,
+        authorization_response = request.url,
+   )
+   session['user_token'] = token
+   return 'Thanks for granting us authorization.'
 
 @app.route('/privacy-policy')
 def privacy_policy():
